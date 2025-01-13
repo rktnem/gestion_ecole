44141c5cc23b9a095a2b1e1de478269972973a3e
Nehemia <nehemiarakotondramanantsoa19gmail.com>
2025-01-13 10:42:57 +0300

    Develop the home and sidebar section

---
+++ src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/view/home/home.fxml"));               
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module GestionEcole {
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
